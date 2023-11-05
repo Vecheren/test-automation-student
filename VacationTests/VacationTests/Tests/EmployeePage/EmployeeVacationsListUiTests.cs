a1c2da0db5399c0c9d04a930a3ca567215423f55
mnoskov <mnoskov@skbkontur.ru>
2023-11-06 00:03:35 +0500

    Вынес метод FillClaimCreationPage

--- VacationTests/VacationTests/Tests/EmployeePage/EmployeeVacationsListUiTests.cs
+++ VacationTests/VacationTests/Tests/EmployeePage/EmployeeVacationsListUiTests.cs
@@ -15,22 +15,25 @@ namespace VacationTests.Tests.EmployeePage
             var openEmployeeVacationListPage = Navigation.OpenEmployeeVacationListPage();
             openEmployeeVacationListPage.NoClaimsTextLabel.WaitPresence();
             openEmployeeVacationListPage.ClaimList.WaitAbsence();
-
             var claimCreationPage = openEmployeeVacationListPage.CreateButton.ClickAndOpen<ClaimCreationPage>();
             var vacationStartDate = DateTime.Now.AddDays(6).ToShortDateString();
             var vacationEndDate = DateTime.Now.AddDays(8).ToShortDateString();
-            claimCreationPage.ClaimStartDatePicker.SetValue(vacationStartDate);
-            claimCreationPage.ClaimEndDatePicker.SetValue(vacationEndDate);
-            claimCreationPage.DirectorFioCombobox.Click();
-            claimCreationPage.DirectorFioCombobox.MenuItems.Count.Wait().EqualTo(5);
-            claimCreationPage.DirectorFioCombobox.MenuItems.Single(x => x.IdLabel.Text.Get() == "22410").Click();
-            claimCreationPage.SendButton.ClickAndOpen<EmployeeVacationListPage>();
-
+            FillClaimCreationPage(claimCreationPage, vacationStartDate, vacationEndDate);
             openEmployeeVacationListPage.NoClaimsTextLabel.WaitAbsence();
             var claim = openEmployeeVacationListPage.ClaimList.Items.Wait().Single();
             claim.TitleLink.Text.Wait().EqualTo("Заявление 1");
             claim.PeriodLabel.Text.Wait().EqualTo(vacationStartDate + " - " + vacationEndDate);
             claim.StatusLabel.Text.Wait().EqualTo("На согласовании");
+        }
+        
+        private void FillClaimCreationPage(ClaimCreationPage claimCreationPage, string vacationStartDate, string vacationEndDate, string directorIdLabel = "22410")
+        {
+            claimCreationPage.ClaimStartDatePicker.SetValue(vacationStartDate);
+            claimCreationPage.ClaimEndDatePicker.SetValue(vacationEndDate);
+            claimCreationPage.DirectorFioCombobox.Click();
+            claimCreationPage.DirectorFioCombobox.MenuItems.Count.Wait().EqualTo(5);
+            claimCreationPage.DirectorFioCombobox.MenuItems.Single(x => x.IdLabel.Text.Get() == directorIdLabel).Click();
+            claimCreationPage.SendButton.ClickAndOpen<EmployeeVacationListPage>();
         }
     }
 }
