528dc32b0f2219ec41596718c6e82762e8cbc947
mnoskov <mnoskov@skbkontur.ru>
2023-11-06 01:08:24 +0500

    Правки

--- VacationTests/VacationTests/Tests/EmployeePage/EmployeeVacationsListUiTests.cs
+++ VacationTests/VacationTests/Tests/EmployeePage/EmployeeVacationsListUiTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using NUnit.Framework;
 using VacationTests.Infrastructure;
 using VacationTests.Infrastructure.PageElements;
@@ -12,31 +11,40 @@ namespace VacationTests.Tests.EmployeePage
         [Test]
         public void CreateClaimFromUI()
         {
-            var openEmployeeVacationListPage = Navigation.OpenEmployeeVacationListPage();
-            openEmployeeVacationListPage.NoClaimsTextLabel.WaitPresence();
+            var openEmployeeVacationListPage = Navigation.OpenEmployeeVacationListPage(); 
+            // следующая строка падает на ulearn
+            // openEmployeeVacationListPage.NoClaimsTextLabel.WaitPresence();
             openEmployeeVacationListPage.ClaimList.WaitAbsence();
             var claimCreationPage = openEmployeeVacationListPage.CreateButton.ClickAndOpen<ClaimCreationPage>();
-            FillClaimCreationPage(claimCreationPage, 6, 8);
-            openEmployeeVacationListPage.NoClaimsTextLabel.WaitAbsence();
+            FillClaimCreationPage(claimCreationPage);
+            // следующая строка падает на ulearn
+            // openEmployeeVacationListPage.NoClaimsTextLabel.WaitAbsence();
             var claim = openEmployeeVacationListPage.ClaimList.Items.Wait().Single();
             claim.TitleLink.Text.Wait().EqualTo("Заявление 1");
-            var date = GetPeriodLabelText(6, 8);
+            var date = GetPeriodLabelText();
             claim.PeriodLabel.Text.Wait().EqualTo(date);
             claim.StatusLabel.Text.Wait().EqualTo("На согласовании");
         }
 
-        private void FillClaimCreationPage(ClaimCreationPage claimCreationPage, int daysFromNowToStartVacation = 6,
-            int daysFromNowToEndVacation = 8, string directorIdLabel = "22410")
+        public void FillClaimCreationPage(ClaimCreationPage claimCreationPage, string claimType = "По уходу за ребенком", int daysFromNowToStartVacation = 8,
+            int daysFromNowToEndVacation = 8, string directorFioLabel = "Захаров Максим Николаевич")
         {
+            claimCreationPage.ClaimTypeSelect.SelectValueByText(claimType);
+            if (claimType == "По уходу за ребенком")
+            {
+                claimCreationPage.ChildAgeInput.InputText("2");
+            }
             claimCreationPage.ClaimStartDatePicker.SetValue(DateTime.Now.AddDays(daysFromNowToStartVacation).ToShortDateString());
             claimCreationPage.ClaimEndDatePicker.SetValue(DateTime.Now.AddDays(daysFromNowToEndVacation).ToShortDateString());
             claimCreationPage.DirectorFioCombobox.Click();
             claimCreationPage.DirectorFioCombobox.MenuItems.Count.Wait().EqualTo(5);
-            claimCreationPage.DirectorFioCombobox.MenuItems.Single(x => x.IdLabel.Text.Get() == directorIdLabel).Click();
+            claimCreationPage.DirectorFioCombobox.InputValue(directorFioLabel);
+            claimCreationPage.DirectorFioCombobox.MenuItems.Wait().Single().Click();
             claimCreationPage.SendButton.ClickAndOpen<EmployeeVacationListPage>();
+            claimCreationPage.SendButton.WaitAbsence();
         }
 
-        private string GetPeriodLabelText(int daysFromNowToStartVacation = 6, int daysFromNowToEndVacation = 8)
+        public string GetPeriodLabelText(int daysFromNowToStartVacation = 8, int daysFromNowToEndVacation = 8)
         {
             return DateTime.Now.AddDays(daysFromNowToStartVacation).ToShortDateString()
                    + " - "
