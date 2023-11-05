e4eb9fa7f12de2da0ab7a7bf522b8766f04330b3
mnoskov <mnoskov@skbkontur.ru>
2023-11-06 00:27:40 +0500

    Правки

--- VacationTests/VacationTests/Tests/EmployeePage/EmployeeVacationsListUiTests.cs
+++ VacationTests/VacationTests/Tests/EmployeePage/EmployeeVacationsListUiTests.cs
@@ -16,24 +16,31 @@ namespace VacationTests.Tests.EmployeePage
             openEmployeeVacationListPage.NoClaimsTextLabel.WaitPresence();
             openEmployeeVacationListPage.ClaimList.WaitAbsence();
             var claimCreationPage = openEmployeeVacationListPage.CreateButton.ClickAndOpen<ClaimCreationPage>();
-            var vacationStartDate = DateTime.Now.AddDays(6).ToShortDateString();
-            var vacationEndDate = DateTime.Now.AddDays(8).ToShortDateString();
-            FillClaimCreationPage(claimCreationPage, vacationStartDate, vacationEndDate);
+            FillClaimCreationPage(claimCreationPage, 6, 8);
             openEmployeeVacationListPage.NoClaimsTextLabel.WaitAbsence();
             var claim = openEmployeeVacationListPage.ClaimList.Items.Wait().Single();
             claim.TitleLink.Text.Wait().EqualTo("Заявление 1");
-            claim.PeriodLabel.Text.Wait().EqualTo(vacationStartDate + " - " + vacationEndDate);
+            var date = GetPeriodLabelText(6, 8);
+            claim.PeriodLabel.Text.Wait().EqualTo(date);
             claim.StatusLabel.Text.Wait().EqualTo("На согласовании");
         }
-        
-        private void FillClaimCreationPage(ClaimCreationPage claimCreationPage, string vacationStartDate, string vacationEndDate, string directorIdLabel = "22410")
+
+        private void FillClaimCreationPage(ClaimCreationPage claimCreationPage, int daysFromNowToStartVacation = 6,
+            int daysFromNowToEndVacation = 8, string directorIdLabel = "22410")
         {
-            claimCreationPage.ClaimStartDatePicker.SetValue(vacationStartDate);
-            claimCreationPage.ClaimEndDatePicker.SetValue(vacationEndDate);
+            claimCreationPage.ClaimStartDatePicker.SetValue(DateTime.Now.AddDays(daysFromNowToStartVacation).ToShortDateString());
+            claimCreationPage.ClaimEndDatePicker.SetValue(DateTime.Now.AddDays(daysFromNowToEndVacation).ToShortDateString());
             claimCreationPage.DirectorFioCombobox.Click();
             claimCreationPage.DirectorFioCombobox.MenuItems.Count.Wait().EqualTo(5);
             claimCreationPage.DirectorFioCombobox.MenuItems.Single(x => x.IdLabel.Text.Get() == directorIdLabel).Click();
             claimCreationPage.SendButton.ClickAndOpen<EmployeeVacationListPage>();
+        }
+
+        private string GetPeriodLabelText(int daysFromNowToStartVacation = 6, int daysFromNowToEndVacation = 8)
+        {
+            return DateTime.Now.AddDays(daysFromNowToStartVacation).ToShortDateString()
+                   + " - "
+                   + DateTime.Now.AddDays(daysFromNowToEndVacation).ToShortDateString();
         }
     }
 }
