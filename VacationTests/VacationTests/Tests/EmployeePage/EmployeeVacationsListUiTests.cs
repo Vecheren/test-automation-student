0b77182e2b38f0225f8282f9f31ab04474b64c84
mnoskov <mnoskov@skbkontur.ru>
2023-11-07 00:30:43 +0500

    Новая версия задания 4

--- VacationTests/VacationTests/Tests/EmployeePage/EmployeeVacationsListUiTests.cs
+++ VacationTests/VacationTests/Tests/EmployeePage/EmployeeVacationsListUiTests.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Kontur.Selone.Properties;
 using NUnit.Framework;
+using VacationTests.Claims;
 using VacationTests.Infrastructure;
 using VacationTests.Infrastructure.PageElements;
-using VacationTests.PageElements;
 using VacationTests.PageObjects;
 
 namespace VacationTests.Tests.EmployeePage
@@ -12,40 +15,68 @@ namespace VacationTests.Tests.EmployeePage
         [Test]
         public void CreateClaimFromUI()
         {
-            var claimCreationPage = Navigation.OpenEmployeeVacationListPage().CreateButton.ClickAndOpen<ClaimCreationPage>();
-            // вместо прошлой строки можно сделать переход по URL заявления Urls.LoginPage + "vacation/1"
-            // но на юлерне 'Navigation.OpenPage<TPageObject>(string)' is inaccessible due to its protection level 
-            var employeeVacationListPage = FillClaimCreationPage(claimCreationPage);
-            var claim = employeeVacationListPage.ClaimList.Items.Wait().Single();
-            CheckClaimOnVacationListPage(claim);
+            var startAndEndDates = new[]
+            {
+                (DateTime.Now.Date.AddDays(100), DateTime.Now.Date.AddDays(110)),
+                (DateTime.Now.Date.AddDays(50), DateTime.Now.Date.AddDays(55))
+            };
+            var employeeVacationListPage = Navigation.OpenEmployeeVacationListPage();
+            
+            FillClaimCreationPage(employeeVacationListPage, ClaimType.Child, startAndEndDates[0]);
+            FillClaimCreationPage(employeeVacationListPage, ClaimType.Child, startAndEndDates[1]);
+            
+            var expected = new[]
+            {
+                ("Заявление 1", GetPeriodTextFromDates(startAndEndDates[0]), ClaimStatus.NonHandled.GetDescription()),
+                ("Заявление 2", GetPeriodTextFromDates(startAndEndDates[1]), ClaimStatus.NonHandled.GetDescription())
+            };
+            CheckClaimOnVacationListPage(employeeVacationListPage, true, expected);
         }
 
-        private void CheckClaimOnVacationListPage(EmployeeClaimItem claim, int claimNumber = 1, int daysToStartVacation = 8, int daysToEndVacation = 8, string status = "На согласовании")
-        {
-            claim.TitleLink.Text.Wait().EqualTo($"Заявление {claimNumber}");
-            claim.PeriodLabel.Text.Wait().EqualTo(GetPeriodLabelText(daysToStartVacation, daysToEndVacation));
-            claim.StatusLabel.Text.Wait().EqualTo(status);
-        }
+        private string GetPeriodTextFromDates((DateTime, DateTime) dates) => 
+            dates.Item1.ToString("dd.MM.yyyy") + " - " + dates.Item2.ToString("dd.MM.yyyy");
 
-        private EmployeeVacationListPage FillClaimCreationPage(ClaimCreationPage claimCreationPage, string claimType = "По уходу за ребенком", int daysToStartVacation = 8,
-            int daysToEndVacation = 8, string directorFioLabel = "Захаров Максим Николаевич")
+        private void CheckClaimOnVacationListPage(EmployeeVacationListPage page, bool ignoreClaimsOrder, params (string, string, string)[] expected)
         {
-            claimCreationPage.ClaimTypeSelect.SelectValueByText(claimType);
-            if (claimType == "По уходу за ребенком")
+            var claims = page.ClaimList.Items
+                .Select(claim => Props.Create(claim.TitleLink.Text, claim.PeriodLabel.Text, claim.StatusLabel.Text))
+                .Wait();
+            if (ignoreClaimsOrder)
+            {
+                claims.EquivalentTo(expected);
+            }
+            else
+            {
+                claims.EqualTo(expected);
+            }
+        }
+        
+        private EmployeeVacationListPage FillClaimCreationPage(EmployeeVacationListPage employeeVacationListPage, ClaimType claimType, (DateTime, DateTime) startAndEndDate, string directorFioLabel = "Захаров Максим Николаевич")
+        {
+            var claimCreationPage = employeeVacationListPage.CreateButton.ClickAndOpen<ClaimCreationPage>();
+            claimCreationPage.ClaimTypeSelect.SelectValueByText(claimType.GetDescription());
+            if (claimType == ClaimType.Child)
             {
                 claimCreationPage.ChildAgeInput.InputText("2");
             }
-            claimCreationPage.ClaimStartDatePicker.SetValue(DateTime.Now.AddDays(daysToStartVacation).ToString("dd.MM.yyyy"));
-            claimCreationPage.ClaimEndDatePicker.SetValue(DateTime.Now.AddDays(daysToEndVacation).ToString("dd.MM.yyyy"));
+            claimCreationPage.ClaimStartDatePicker.SetValue(startAndEndDate.Item1.ToString("dd.MM.yyyy"));
+            claimCreationPage.ClaimEndDatePicker.SetValue(startAndEndDate.Item2.ToString("dd.MM.yyyy"));
             claimCreationPage.DirectorFioCombobox.SelectValue(directorFioLabel);
             return claimCreationPage.SendButton.ClickAndOpen<EmployeeVacationListPage>();
         }
 
-        private string GetPeriodLabelText(int daysFromStartVacation = 8, int daysToEndVacation = 8)
+
+        /*[Test]
+        public void CreateVacations_ShouldAddItemsToClaimsList()
         {
-            return DateTime.Now.AddDays(daysFromStartVacation).ToString("dd.MM.yyyy")
-                   + " - "
-                   + DateTime.Now.AddDays(daysToEndVacation).ToString("dd.MM.yyyy");
-        }
+            var claimCreationPage = Navigation.OpenEmployeeVacationListPage().CreateButton.ClickAndOpen<ClaimCreationPage>();
+            var employeeVacationListPage = FillClaimCreationPage(claimCreationPage);
+            employeeVacationListPage.CreateButton.ClickAndOpen<ClaimCreationPage>();
+            FillClaimCreationPage(claimCreationPage);
+            // проходит, но надо по хорошему сделать
+            CheckClaimOnVacationListPage(employeeVacationListPage.ClaimList.Items.Wait().GetValue().First());
+            CheckClaimOnVacationListPage(employeeVacationListPage.ClaimList.Items.Wait().GetValue().Last(), 2);
+            
+        }*/
     }
 }
